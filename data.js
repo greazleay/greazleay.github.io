690a7db6a60d83457d0092e32a12c224c9abd78a
Lekan Adetunmbi <lothbroch@gmail.com>
2021-09-14 15:00:24 +0100

    Layout Complete, pending styling

---
+++ data.js
@@ -0,0 +1,191 @@
+export const vanilla_list = [
+    {
+        projectName: 'Rock Paper Scissors',
+        links: [
+            {
+                url: 'https://github.com/greazleay/rock-paper-scissors/',
+                text: 'View Code'
+            },
+            {
+                url: 'https://greazleay.github.io/rock-paper-scissors/',
+                text: 'Live Preview'
+            }
+        ]
+    },
+    {
+        projectName: 'Etch A Sketch',
+        links: [
+            {
+                url: 'https://github.com/greazleay/etch-a-sketch/',
+                text: 'View Code'
+            },
+            {
+                url: 'https://greazleay.github.io/etch-a-sketch/',
+                text: 'Live Preview'
+            }
+        ]
+    },
+    {
+        projectName: 'Basic Calculator',
+        links: [
+            {
+                url: 'https://github.com/greazleay/basic-calculator/',
+                text: 'View Code'
+            },
+            {
+                url: 'https://greazleay.github.io/basic-calculator/',
+                text: 'Live Preview'
+            }
+        ]
+    },
+    {
+        projectName: 'Binary Calculator',
+        links: [
+            {
+                url: 'https://github.com/greazleay/binary-calculator/',
+                text: 'View Code'
+            },
+            {
+                url: 'https://greazleay.github.io/binary-calculator/',
+                text: 'Live Preview'
+            }
+        ]
+    },
+    {
+        projectName: 'Book Library',
+        links: [
+            {
+                url: 'https://github.com/greazleay/book-library/',
+                text: 'View Code'
+            },
+            {
+                url: 'https://greazleay.github.io/book-library/',
+                text: 'Live Preview'
+            }
+        ]
+    },
+    {
+        projectName: 'Tic Tac Toe',
+        links: [
+            {
+                url: 'https://github.com/greazleay/tic-tac-toe/',
+                text: 'View Code'
+            },
+            {
+                url: 'https://greazleay.github.io/tic-tac-toe/',
+                text: 'Live Preview'
+            }
+        ]
+    },
+    {
+        projectName: 'Restaurant Page',
+        links: [
+            {
+                url: 'https://github.com/greazleay/restaurant/',
+                text: 'View Code'
+            },
+            {
+                url: 'https://greazleay.github.io/restaurant/',
+                text: 'Live Preview'
+            }
+        ]
+    },
+    {
+        projectName: 'Todo List',
+        links: [
+            {
+                url: 'https://github.com/greazleay/todo-list/',
+                text: 'View Code'
+            },
+            {
+                url: 'https://greazleay.github.io/todo-list/',
+                text: 'Live Preview'
+            }
+        ]
+    },
+    {
+        projectName: 'Weather App',
+        links: [
+            {
+                url: 'https://github.com/greazleay/weather-app/',
+                text: 'View Code'
+            },
+            {
+                url: 'https://greazleay.github.io/weather-app/',
+                text: 'Live Preview'
+            }
+        ]
+    }
+]
+
+export const react_list = [
+    {
+        projectName: 'Resume Builder',
+        links: [
+            {
+                url: 'https://github.com/greazleay/cv-app/',
+                text: 'View Code'
+            },
+            {
+                url: 'https://greazleay.github.io/cv-app/',
+                text: 'Live Preview'
+            }
+        ]
+    },
+    {
+        projectName: 'Memory Card Game',
+        links: [
+            {
+                url: 'https://github.com/greazleay/memory-card/',
+                text: 'View Code'
+            },
+            {
+                url: 'https://greazleay.github.io/memory-card/',
+                text: 'Live Preview'
+            }
+        ]
+    },
+    {
+        projectName: 'Shopping Cart',
+        links: [
+            {
+                url: 'https://github.com/greazleay/shopping-cart/',
+                text: 'View Code'
+            },
+            {
+                url: 'https://greazleay.github.io/shopping-cart/',
+                text: 'Live Preview'
+            }
+        ]
+    }
+]
+
+export const full_stack = [
+    {
+        projectName: 'Basic Informational Site',
+        links: [
+            {
+                url: 'https://github.com/greazleay/basic-informational-site/',
+                text: 'View Code'
+            },
+            {
+                url: 'https://basic-informational-site.greazleay.repl.co/',
+                text: 'Live Preview'
+            }
+        ]
+    },
+    {
+        projectName: 'Mini Message Board',
+        links: [
+            {
+                url: 'https://github.com/greazleay/shopping-cart/',
+                text: 'View Code'
+            },
+            {
+                url: 'https://rocky-fortress-40972.herokuapp.com',
+                text: 'Live Preview'
+            }
+        ]
+    }
+]
+
---
+++ index.js
@@ -0,0 +1,68 @@
+import { vanilla_list, react_list, full_stack } from "./data.js";
+
+// Components
+
+const anchorTag = (url, txt, parent) => {
+    const a = document.createElement('a');
+    a.href = url;
+    a.target = '_blank';
+    a.rel = 'noopener noreferrer';
+    a.textContent = txt;
+    parent.appendChild(a);
+}
+
+const headline = (e, txt, url) => {
+    const parent = document.createElement(e);
+    parent.append(txt)
+    const a = document.createElement('a');
+    anchorTag(url, 'here', parent)
+    parent.append(' ...')
+    container.appendChild(parent);
+}
+
+const addPara = (txt, parent) => {
+    const p = document.createElement('p');
+    p.textContent = txt
+    parent.appendChild(p)
+}
+
+// Rendered
+
+const container = document.createElement('div');
+container.className = 'container';
+document.body.appendChild(container);
+
+const header = document.createElement('header');
+const img = document.createElement('img');
+img.src = './favicon-32x32.png';
+img.alt = 'img';
+header.appendChild(img)
+container.appendChild(header);
+
+headline('h1', 'My Resume is available ', 'https://drive.google.com/file/d/1AAf35x79zF83QMD3xlKDajH-FI5EnGEV/view?usp=sharing')
+addPara('Foundation Projects I have done and still updating are listed below:', container)
+
+const projects = document.createElement('div');
+projects.className = 'projects';
+container.appendChild(projects);
+
+const project = (pname, links) => {
+    const div = document.createElement('div');
+    div.className = 'project';
+    const span = document.createElement('span');
+    span.textContent = pname;
+    div.appendChild(span);
+    links.forEach(link => anchorTag(link.url, link.text, div));
+    projects.appendChild(div)
+}
+
+addPara('VanillaJS Projects', projects)
+vanilla_list.forEach(entry => project(entry.projectName, entry.links))
+
+addPara('ReactJS Projects', projects)
+react_list.forEach(entry => project(entry.projectName, entry.links))
+
+addPara('Full Stack NodeJS', projects)
+full_stack.forEach(entry => project(entry.projectName, entry.links))
+
+headline('p', 'You can view my Hackerrank profile ', 'https://www.hackerrank.com/greazleay')
