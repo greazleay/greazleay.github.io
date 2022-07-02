d5efbddd3667a54abb2160b04f6ba210bce1436d
Lekan Adetunmbi <lothbroch@gmail.com>
2022-07-02 10:42:05 +0100

    Further Remodelling with Project Categorization and Major Data Structure Modifications

--- data.js
+++ data.js
@@ -1,311 +1,335 @@
 'use strict';
 
-export const static_list = [
-    {
-        projectName: 'Google Clone',
-        links: [
-            {
-                url: 'https://github.com/greazleay/googleclone/',
-                text: 'View Code'
-            },
-            {
-                url: 'https://greazleay.github.io/googleclone/',
-                text: 'Live Preview'
-            }
-        ]
-    }
-]
+export const static_list = {
+    categoryName: 'Static Websites',
+    projectList: [
+        {
+            projectName: 'Google Clone',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/googleclone/',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://greazleay.github.io/googleclone/',
+                    text: 'Live Preview'
+                }
+            ]
+        }
+    ]
+}
 
-export const vanilla_list = [
-    {
-        projectName: 'Etch A Sketch',
-        links: [
-            {
-                url: 'https://github.com/greazleay/etch-a-sketch/',
-                text: 'View Code'
-            },
-            {
-                url: 'https://greazleay.github.io/etch-a-sketch/',
-                text: 'Live Preview'
-            }
-        ]
-    },
-    {
-        projectName: 'Rock Paper Scissors',
-        links: [
-            {
-                url: 'https://github.com/greazleay/rock-paper-scissors/',
-                text: 'View Code'
-            },
-            {
-                url: 'https://greazleay.github.io/rock-paper-scissors/',
-                text: 'Live Preview'
-            }
-        ]
-    },
-    {
-        projectName: 'Basic Calculator',
-        links: [
-            {
-                url: 'https://github.com/greazleay/basic-calculator/',
-                text: 'View Code'
-            },
-            {
-                url: 'https://greazleay.github.io/basic-calculator/',
-                text: 'Live Preview'
-            }
-        ]
-    },
-    {
-        projectName: 'Binary Calculator',
-        links: [
-            {
-                url: 'https://github.com/greazleay/binary-calculator/',
-                text: 'View Code'
-            },
-            {
-                url: 'https://greazleay.github.io/binary-calculator/',
-                text: 'Live Preview'
-            }
-        ]
-    },
-    {
-        projectName: 'Book Library',
-        links: [
-            {
-                url: 'https://github.com/greazleay/book-library/',
-                text: 'View Code'
-            },
-            {
-                url: 'https://greazleay.github.io/book-library/',
-                text: 'Live Preview'
-            }
-        ]
-    },
-    {
-        projectName: 'Tic Tac Toe',
-        links: [
-            {
-                url: 'https://github.com/greazleay/tic-tac-toe/',
-                text: 'View Code'
-            },
-            {
-                url: 'https://greazleay.github.io/tic-tac-toe/',
-                text: 'Live Preview'
-            }
-        ]
-    },
-    {
-        projectName: 'Weather App',
-        links: [
-            {
-                url: 'https://github.com/greazleay/weather-app/',
-                text: 'View Code'
-            },
-            {
-                url: 'https://greazleay.github.io/weather-app/',
-                text: 'Live Preview'
-            }
-        ]
-    },
-    {
-        projectName: 'Restaurant Page',
-        links: [
-            {
-                url: 'https://github.com/greazleay/restaurant/',
-                text: 'View Code'
-            },
-            {
-                url: 'https://greazleay.github.io/restaurant/',
-                text: 'Live Preview'
-            }
-        ]
-    },
-    {
-        projectName: 'Todo List',
-        links: [
-            {
-                url: 'https://github.com/greazleay/todo-list/',
-                text: 'View Code'
-            },
-            {
-                url: 'https://greazleay.github.io/todo-list/',
-                text: 'Live Preview'
-            }
-        ]
-    }
-]
+export const vanilla_list = {
+    categoryName: 'Vanilla JS Projects',
+    projectList: [
+        {
+            projectName: 'Etch A Sketch',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/etch-a-sketch/',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://greazleay.github.io/etch-a-sketch/',
+                    text: 'Live Preview'
+                }
+            ]
+        },
+        {
+            projectName: 'Rock Paper Scissors',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/rock-paper-scissors/',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://greazleay.github.io/rock-paper-scissors/',
+                    text: 'Live Preview'
+                }
+            ]
+        },
+        {
+            projectName: 'Basic Calculator',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/basic-calculator/',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://greazleay.github.io/basic-calculator/',
+                    text: 'Live Preview'
+                }
+            ]
+        },
+        {
+            projectName: 'Binary Calculator',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/binary-calculator/',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://greazleay.github.io/binary-calculator/',
+                    text: 'Live Preview'
+                }
+            ]
+        },
+        {
+            projectName: 'Book Library',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/book-library/',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://greazleay.github.io/book-library/',
+                    text: 'Live Preview'
+                }
+            ]
+        },
+        {
+            projectName: 'Tic Tac Toe',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/tic-tac-toe/',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://greazleay.github.io/tic-tac-toe/',
+                    text: 'Live Preview'
+                }
+            ]
+        },
+        {
+            projectName: 'Weather App',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/weather-app/',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://greazleay.github.io/weather-app/',
+                    text: 'Live Preview'
+                }
+            ]
+        },
+        {
+            projectName: 'Restaurant Page',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/restaurant/',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://greazleay.github.io/restaurant/',
+                    text: 'Live Preview'
+                }
+            ]
+        },
+        {
+            projectName: 'Todo List',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/todo-list/',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://greazleay.github.io/todo-list/',
+                    text: 'Live Preview'
+                }
+            ]
+        }
+    ]
+}
 
-export const react_list = [
-    {
-        projectName: 'Members Only',
-        links: [
-            {
-                url: 'https://github.com/greazleay/members-only',
-                text: 'View Code'
-            },
-            {
-                url: 'https://mema.azurewebsites.net',
-                text: 'Live Preview'
-            }
-        ]
-    },
-    {
-        projectName: 'Inventory App',
-        links: [
-            {
-                url: 'https://github.com/greazleay/inventory-app',
-                text: 'View Code'
-            },
-            {
-                url: 'https://pollaroid.net/inventory-app',
-                text: 'Live Preview'
-            }
-        ]
-    },
-    {
-        projectName: 'Shopping Cart',
-        links: [
-            {
-                url: 'https://github.com/greazleay/shopping-cart/',
-                text: 'View Code'
-            },
-            {
-                url: 'https://greazleay.github.io/shopping-cart/',
-                text: 'Live Preview'
-            }
-        ]
-    },
-    {
-        projectName: 'Memory Card Game',
-        links: [
-            {
-                url: 'https://github.com/greazleay/memory-card/',
-                text: 'View Code'
-            },
-            {
-                url: 'https://greazleay.github.io/memory-card/',
-                text: 'Live Preview'
-            }
-        ]
-    },
-    {
-        projectName: 'Resume Builder',
-        links: [
-            {
-                url: 'https://github.com/greazleay/resume-builder',
-                text: 'View Code'
-            },
-            {
-                url: 'https://resume-builder-ashy.vercel.app/',
-                text: 'Live Preview'
-            }
-        ]
-    }
-]
+export const react_list = {
+    categoryName: 'React JS/TS Projects',
+    projectList: [
+        {
+            projectName: 'Members Only',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/members-only',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://mema.azurewebsites.net',
+                    text: 'Live Preview'
+                }
+            ]
+        },
+        {
+            projectName: 'Inventory App',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/inventory-app',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://pollaroid.net/inventory-app',
+                    text: 'Live Preview'
+                }
+            ]
+        },
+        {
+            projectName: 'Shopping Cart',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/shopping-cart/',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://greazleay.github.io/shopping-cart/',
+                    text: 'Live Preview'
+                }
+            ]
+        },
+        {
+            projectName: 'Memory Card Game',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/memory-card/',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://greazleay.github.io/memory-card/',
+                    text: 'Live Preview'
+                }
+            ]
+        },
+        {
+            projectName: 'Resume Builder',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/resume-builder',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://resume-builder-ashy.vercel.app/',
+                    text: 'Live Preview'
+                }
+            ]
+        }
+    ]
+}
 
-export const nodejs_full_stack = [
-    {
-        projectName: 'Mini Message Board',
-        links: [
-            {
-                url: 'https://github.com/greazleay/mini-message-board/',
-                text: 'View Code'
-            },
-            {
-                url: 'https://mini-message-board.greazleay.repl.co/',
-                text: 'Live Preview'
-            }
-        ]
-    },
-    {
-        projectName: 'Basic Informational Site',
-        links: [
-            {
-                url: 'https://github.com/greazleay/basic-informational-site/',
-                text: 'View Code'
-            },
-            {
-                url: 'https://basic-informational-site.greazleay.repl.co/',
-                text: 'Live Preview'
-            }
-        ]
-    }
-]
+export const nodejs_full_stack = {
+    categoryName: 'Full Stack Node JS',
+    projectList: [
+        {
+            projectName: 'Mini Message Board',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/mini-message-board/',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://mini-message-board.greazleay.repl.co/',
+                    text: 'Live Preview'
+                }
+            ]
+        },
+        {
+            projectName: 'Basic Informational Site',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/basic-informational-site/',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://basic-informational-site.greazleay.repl.co/',
+                    text: 'Live Preview'
+                }
+            ]
+        }
+    ]
+}
 
-export const nestjs_api_list = [
-    {
-        projectName: 'Thrifty API',
-        links: [
-            {
-                url: 'https://github.com/greazleay/thrifty-api',
-                text: 'View Code'
-            },
-            {
-                url: 'https://api-thrifty.herokuapp.com/api-docs',
-                text: 'API Documentation'
-            }
-        ]
-    }
-]
+export const nestjs_api_list = {
+    categoryName: 'NestJS REST APIs',
+    projectList: [
+        {
+            projectName: 'Thrifty API',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/thrifty-api',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://api-thrifty.herokuapp.com/api-docs',
+                    text: 'API Documentation'
+                }
+            ]
+        }
+    ]
+}
 
-export const express_api_list = [
-    {
-        projectName: 'Members Only API',
-        links: [
-            {
-                url: 'https://github.com/greazleay/members-only-api',
-                text: 'View Code'
-            },
-            {
-                url: 'https://api-mbo.herokuapp.com/api-docs',
-                text: 'API Documentation'
-            }
-        ]
-    },
-    {
-        projectName: 'Order Eat API',
-        links: [
-            {
-                url: 'https://github.com/greazleay/order-eat-api',
-                text: 'View Code'
-            },
-            {
-                url: 'https://api-ordereat.herokuapp.com',
-                text: 'Base URL'
-            }
-        ]
-    }
-]
+export const express_api_list = {
+    categoryName: 'Express REST APIs',
+    projectList: [
+        {
+            projectName: 'Members Only API',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/members-only-api',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://api-mbo.herokuapp.com/api-docs',
+                    text: 'API Documentation'
+                }
+            ]
+        },
+        {
+            projectName: 'Order Eat API',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/order-eat-api',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://api-ordereat.herokuapp.com',
+                    text: 'Base URL'
+                }
+            ]
+        }
+    ]
+}
 
-export const fastify_api_list = [
-    {
-        projectName: 'Inventory App API',
-        links: [
-            {
-                url: 'https://github.com/greazleay/inventory-app-api',
-                text: 'View Code'
-            },
-            {
-                url: 'https://inv-hub.herokuapp.com/api',
-                text: 'Base URL'
-            }
-        ]
-    }
-]
+export const fastify_api_list = {
+    categoryName: 'Fastify REST APIs',
+    projectList: [
+        {
+            projectName: 'Inventory App API',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/inventory-app-api',
+                    text: 'View Code'
+                },
+                {
+                    url: 'https://inv-hub.herokuapp.com/api',
+                    text: 'Base URL'
+                }
+            ]
+        }
+    ]
+}
 
-export const spring_api_list = [
-    {
-        projectName: 'Book Library API',
-        links: [
-            {
-                url: 'https://github.com/greazleay/booklibraryapi',
-                text: 'View Code'
-            },
-            {
-                url: '#',
-                text: 'Local Install Only'
-            }
-        ]
-    }
-]
+export const spring_api_list = {
+    categoryName: 'Spring Boot + Kotlin REST APIs',
+    projectList: [
+        {
+            projectName: 'Book Library API',
+            links: [
+                {
+                    url: 'https://github.com/greazleay/booklibraryapi',
+                    text: 'View Code'
+                },
+                {
+                    url: '#',
+                    text: 'Local Install Only'
+                }
+            ]
+        }
+    ]
+}
--- index.js
+++ index.js
@@ -24,9 +24,10 @@ const anchorTag = (url, txt, parent) => {
 
 const headline = (elm, txt, url, parentTag) => {
     const parentElm = document.createElement(elm);
-    parentElm.append(txt);
-    anchorTag(url, 'here', parentElm);
-    parentElm.append('...');
+    // parentElm.append(txt);
+    // anchorTag(url, 'here', parentElm);
+    // parentElm.append('...');
+    anchorTag(url, txt, parentElm);
     parentTag.appendChild(parentElm);
 };
 
@@ -64,55 +65,54 @@ addElm('h1', 'About Me', main);
 addElm('p', 'Back End Software Engineer with practical skills in building web applications and a keen interest in learning new technologies, also open to Front End roles.', main);
 addElm('p', 'Programming Languages: TypeScript, JavaScript, Kotlin', main);
 
-headline('p', 'View my Hackerrank profile ', 'https://www.hackerrank.com/greazleay', main);
-headline('p', 'View my LeetCode profile ', 'https://www.leetcode.com/greazleay', main);
-headline('p', 'My Resume is available ', 'https://drive.google.com/file/d/1AAf35x79zF83QMD3xlKDajH-FI5EnGEV/view?usp=sharing', main);
+headline('p', 'View my Hackerrank Profile', 'https://www.hackerrank.com/greazleay', main);
+headline('p', 'View my LeetCode Profile', 'https://www.leetcode.com/greazleay', main);
+headline('p', 'View My Resume', 'https://drive.google.com/file/d/1AAf35x79zF83QMD3xlKDajH-FI5EnGEV/view?usp=sharing', main);
+headline('p', 'Connect with me on LinkedIn', 'https://www.linkedin.com/in/siezes', main);
 
-addElm('p', 'The underlisted are personal projects and meant for demonstration purposes only:', main);
+addElm('p', 'The underlisted are personal sandbox projects meant for demonstration purposes only:', main);
 
-const projects = document.createElement('div');
-projects.className = 'grid';
-main.appendChild(projects);
+const projectCategory = (projectData) => {
 
-const project = (projectName, links) => {
-    const div = document.createElement('div');
-    div.className = 'card';
+    const { categoryName, projectList } = projectData;
 
-    addElm('h3', projectName, div);
+    const category = document.createElement('div');
+    category.className = 'category';
 
-    const img = document.createElement('img');
-    img.src = './web2.png'
-    img.width = '128'
-    img.height = '128'
-    div.appendChild(img)
-    
-    links.forEach(link => anchorTag(link.url, link.text, div));
+    const categoryTitle = document.createElement('div');
+    addElm('p', categoryName, categoryTitle);
+    category.appendChild(categoryTitle);
 
-    projects.appendChild(div);
+    const projects = document.createElement('div');
+    projects.className = 'grid';
+    category.appendChild(projects);
+
+    const project = (projectName, links) => {
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        addElm('h3', projectName, card);
+
+        const img = document.createElement('img');
+        img.src = './web2.png'
+        img.width = '128'
+        img.height = '128'
+        card.appendChild(img)
+
+        links.forEach(link => anchorTag(link.url, link.text, card));
+        projects.appendChild(card);
+    }
+
+    projectList.forEach(data => project(data.projectName, data.links));
+
+    main.appendChild(category)
 };
 
-// addElm('p', 'NestJS REST APIs', projects);
-nestjs_api_list.forEach(entry => project(entry.projectName, entry.links));
-
-// addElm('p', 'Express REST APIs', projects);
-express_api_list.forEach(entry => project(entry.projectName, entry.links));
-
-// addElm('p', 'Fastify REST APIs', projects);
-fastify_api_list.forEach(entry => project(entry.projectName, entry.links));
-
-// addElm('p', 'Spring Boot + Kotlin REST APIs', projects);
-spring_api_list.forEach(entry => project(entry.projectName, entry.links));
-
-// addElm('p', 'Full Stack Node JS', projects);
-nodejs_full_stack.forEach(entry => project(entry.projectName, entry.links));
-
-// addElm('p', 'FRONT END:', projects);
-
-// addElm('p', 'React JS/TS Projects', projects);
-react_list.forEach(entry => project(entry.projectName, entry.links));
-
-// addElm('p', 'Vanilla JS Projects', projects);
-vanilla_list.forEach(entry => project(entry.projectName, entry.links));
-
-// addElm('p', 'Static Websites', projects);
-static_list.forEach(entry => project(entry.projectName, entry.links));
+projectCategory(nestjs_api_list)
+projectCategory(express_api_list)
+projectCategory(fastify_api_list)
+projectCategory(spring_api_list)
+projectCategory(nodejs_full_stack)
+projectCategory(react_list)
+projectCategory(vanilla_list)
+projectCategory(static_list)
